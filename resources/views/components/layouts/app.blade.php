a036595f6c20d4e5b1c70e11f563189237a392f8
vernandaspw <vernandaspw@gmail.com>
2024-01-06 21:18:17 +0700

    aa

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -18,7 +18,7 @@
     <!-- Bootstrap icons-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" rel="stylesheet" />
     <!-- Core theme CSS (includes Bootstrap)-->
-    @livewireStyles
+
     <link href="{{ asset('page/css/styles.css') }}" rel="stylesheet" />
 
     <style>
@@ -58,7 +58,7 @@
     </footer>
 
 
-    @livewireScripts
+
 
     <!-- Bootstrap core JS-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
